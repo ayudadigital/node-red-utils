133da272badedd99b11ebdcb572da386db13df31
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-05-28 19:21:13 +0200

    New: Add join data script

---
+++ join-node-red-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+echo >&2 "Nodered join data (c) Ayuda Digital 2020"
+echo >&2 ""
+
+if [ $# -ne 1 ]; then
+    echo >&2 "Usage: join-node-red-data.sh [input_directory]"
+    echo >&2
+    echo >&2 "Example:"
+    echo >&2 "   nodered-split-data.sh items > data/flows.json"
+    echo >&2
+    echo >&2 "This will join all json files within 'items' directory into 'data/flows.json'"
+    echo >&2 "The process will overwrite the target file"
+    exit 1
+fi
+
+inputDirectory=$(echo "$(cd "$1"; pwd)")
+echo >&2 -ne "\t# Input data files check '${inputDirectory}': "
+
+jq empty "$inputDirectory"/*.json || (echo >&2 "Aborting"; exit 1)
+echo >&2 "OK"
+
+echo >&2
+echo >&2 -ne "# Join json files: "
+jq . "$inputDirectory"/*.json
+echo >&2 "OK"
+
+echo >&2 "# FINISH"
