bd4b48f5f63a5093a62a47a0a6aa009941e9c5f2
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-05-28 19:28:24 +0200

    Update: Review scripts, use stderr output

--- join-node-red-data.sh
+++ join-node-red-data.sh
@@ -15,8 +15,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+echo >&2 -ne "# Chech presence of 'jq' tool: "
+which -s jq || (echo >&2 "not installed. Aborting"; exit 1)
+echo >&2 "OK"
+
 inputDirectory=$(echo "$(cd "$1"; pwd)")
-echo >&2 -ne "\t# Input data files check '${inputDirectory}': "
+echo >&2 -ne "# Input data files check '${inputDirectory}': "
 
 jq empty "$inputDirectory"/*.json || (echo >&2 "Aborting"; exit 1)
 echo >&2 "OK"
--- split-node-red-data.sh
+++ split-node-red-data.sh
@@ -1,49 +1,53 @@
 #!/bin/bash
 set -eu
 
-echo "Nodered split data (c) Ayuda Digital 2020"
-echo ""
+echo >&2 "Nodered split data (c) Ayuda Digital 2020"
+echo >&2 ""
 
 if [ $# -ne 2 ]; then
-    echo "Usage: split-node-red-data.sh [nodered_data_file] [output_directory]"
-    echo
-    echo "Example:"
-    echo "   nodered-split-data.sh data/flows.json items"
-    echo
-    echo "This will split the data/flows.json node-red data file into individual json files (one per tab) saving they into 'items' directory"
-    echo "All json files in 'itens' directory will be removed before the split process"
+    echo >&2 "Usage: split-node-red-data.sh [nodered_data_file] [output_directory]"
+    echo >&2
+    echo >&2 "Example:"
+    echo >&2 "   nodered-split-data.sh data/flows.json items"
+    echo >&2
+    echo >&2 "This will split the data/flows.json node-red data file into individual json files (one per tab) saving they into 'items' directory"
+    echo >&2 "All json files in 'itens' directory will be removed before the split process"
     exit 1
 fi
 
+echo >&2 -ne "# Chech presence of 'jq' tool: "
+which -s jq || (echo >&2 "not installed. Aborting"; exit 1)
+echo >&2 "OK"
+
 inputFile=$(echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")")
-echo -ne "\t# Input data file check '${inputFile}': "
-jq empty $inputFile  || (echo "Aborting"; exit 1)
+echo >&2 -ne "# Input data file check '${inputFile}': "
+jq empty $inputFile  || (echo >&2 "Aborting"; exit 1)
 inputDirectory=$(echo "$(cd "$(dirname "$inputFile")"; pwd)")
-echo "OK"
+echo >&2 "OK"
 outputDirectory=$(echo "$(cd "$2"; pwd)")
-echo -ne "\t# Output directory check '${outputDirectory}': "
-[ ! -d $outputDirectory ] && (echo "does not exist. Aborting"; exit 1)
-[ $inputDirectory == $outputDirectory ] && (echo "Input and output directory are the same. Aborting"; exit 1)
-echo "OK"
-echo
-echo "# Removing all .json files '${outputDirectory}/*.json': "
-echo "=========="
+echo >&2 -ne "# Output directory check '${outputDirectory}': "
+[ ! -d $outputDirectory ] && (echo >&2 "does not exist. Aborting"; exit 1)
+[ $inputDirectory == $outputDirectory ] && (echo >&2 "Input and output directory are the same. Aborting"; exit 1)
+echo >&2 "OK"
+echo >&2
+echo >&2 "# Removing all .json files '${outputDirectory}/*.json': "
+echo >&2 "=========="
 rm -fv "${outputDirectory}"/*.json
-echo "=========="
+echo >&2 "=========="
 
-echo
-echo -n "# Extract the ID of each tab: "
+echo >&2
+echo >&2 -n "# Extract the ID of each tab: "
 idList=$(jq ".[]|select (.type == \"tab\")|.id" $inputFile)
-echo "$(echo $idList|wc -w) tabs found"
+echo >&2 "$(echo $idList|wc -w) tabs found"
 
-echo "# Loop tabs extracting content:"
+echo >&2 "# Loop tabs extracting content:"
 for id in $idList; do
     outputFile="${id%\"}"
     outputFile="${outputDirectory}/${outputFile#\"}".json
-    echo -e "\tTab: ${id} => file: ${outputFile}"
+    echo >&2 -e "\tTab: ${id} => file: ${outputFile}"
     (
         jq ".[]|select ((.id == ${id}) or (.z == ${id}))" $inputFile
     ) | jq -s '.' > "${outputFile}"
 done
 
-echo "# FINISH"
+echo >&2 "# FINISH"
